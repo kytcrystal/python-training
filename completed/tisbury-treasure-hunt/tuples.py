9f7fe337167d275754aec95700dc11c86627647c
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-03 21:40:04 +0200

    Tisbury Treasure Hunt Complete

--- completed/tisbury-treasure-hunt/tuples.py
+++ completed/tisbury-treasure-hunt/tuples.py
@@ -8,7 +8,7 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
@@ -18,7 +18,7 @@ def convert_coordinate(coordinate):
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
@@ -29,7 +29,7 @@ def compare_records(azara_record, rui_record):
     :return: bool - do the coordinates match?
     """
 
-    pass
+    return convert_coordinate(get_coordinate(azara_record)) == get_coordinate(rui_record)
 
 
 def create_record(azara_record, rui_record):
@@ -40,7 +40,9 @@ def create_record(azara_record, rui_record):
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
-    pass
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    return "not a match"
 
 
 def clean_up(combined_record_group):
@@ -54,4 +56,7 @@ def clean_up(combined_record_group):
     (see HINTS.md for an example).
     """
 
-    pass
+    report = ""
+    for record in combined_record_group:
+        report += f"(\'{record[0]}\', \'{record[2]}\', {record[3]}, \'{record[4]}\')\n"
+    return report
