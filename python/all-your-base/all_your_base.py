a6ac21df6c3a844e96b7940e55f39caa80f4a172
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-06 11:31:14 +0200

    All Your Base Refactored

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -15,30 +15,37 @@ def rebase(input_base, digits, output_base):
     if max(digits) == 0:
         return [0]
 
-    # Convert digits to base 10
+    b10_number = convert_to_base_10(input_base, digits)
+        
+    if output_base == 10:
+        return [int(digit) for digit in str(b10_number)]
+   
+    power = determine_max_power(output_base, b10_number)
+    
+    output_digits = convert_to_output_base(output_base, b10_number, power)
+        
+    return output_digits
+
+def convert_to_output_base(output_base, b10_number, power):
+    output_digits = [0 for _ in range(0, power+1)]
+    for index, digit in enumerate(output_digits):
+        power_of_base = output_base ** power
+        coefficient = b10_number // power_of_base
+        output_digits[index] = coefficient
+        b10_number -= coefficient * power_of_base
+        power -= 1
+    return output_digits
+
+def determine_max_power(output_base, b10_number):
+    max_power = 0
+    while b10_number >= output_base:
+        b10_number /= output_base
+        max_power += 1
+    return max_power
+
+def convert_to_base_10(input_base, digits):
     base_10_number = 0
     digits_length = len(digits)
     for index, digit in enumerate(digits):
         base_10_number += digit * (input_base ** (digits_length-1-index))
-        
-    if output_base == 10:
-        return [int(digit) for digit in str(base_10_number)]
-   
-    # Convert digits to output base
-    number = base_10_number
-    max_power = 0
-    while number >= output_base:
-        number /= output_base
-        max_power += 1
-    output_digits = [0 for _ in range(0, max_power+1)]
-    
-    power = max_power
-    number = base_10_number
-    for index, digit in enumerate(output_digits):
-        power_of_base = output_base ** power
-        coefficient = number // power_of_base
-        output_digits[index] = coefficient
-        number -= coefficient * power_of_base
-        power -= 1
-        
-    return output_digits
+    return base_10_number
