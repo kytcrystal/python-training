2b97769414c0c1cff6395bc808133253c9f5a829
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-09 17:41:26 +0200

    Wordy Refactor

--- completed-easy/wordy/wordy.py
+++ completed-easy/wordy/wordy.py
@@ -2,57 +2,58 @@ def answer(question):
     
     if not question.startswith("What is"):
         raise ValueError("unknown operation")
-    if not question.startswith("What is "):
+    question = question.removeprefix("What is").removesuffix("?")
+    if not question: 
         raise ValueError("syntax error")
+    question_in_list = question.split()
     
-    question_in_list = question[:-1].split()[2:]
-    
-    num_a = question_in_list.pop(0)
-    
-    if not num_a.isnumeric():        
-        try:
-            num_a = int(num_a)
-        except:
-            raise ValueError("syntax error")
+    lhs_num = retrieve_next(question_in_list)
+    lhs_num = convert_string_to_int(lhs_num)
     
     operations = ["plus", "minus", "multiplied", "divided"]
     
     while len(question_in_list) > 0:
     
-        operation = question_in_list.pop(0)
-        if operation.isnumeric():
+        operation = retrieve_next(question_in_list)
+        if not operation.isalpha():
             raise ValueError("syntax error")
         if not operation in operations:
             raise ValueError("unknown operation")
         if operation == "multiplied" or operation == "divided":
-            try:
-                by = question_in_list.pop(0)
-                if by != "by":
-                    raise ValueError("syntax error")
-            except:
+            by_word = retrieve_next(question_in_list)
+            if by_word != "by":
                 raise ValueError("syntax error")
         
+        rhs_num = retrieve_next(question_in_list)
+        rhs_num = convert_string_to_int(rhs_num)
+            
+        result = evaluate(lhs_num, operation, rhs_num)
+        lhs_num = result
+
+    return lhs_num
+
+def retrieve_next(question_in_list):
+    try:
+        item = question_in_list.pop(0)
+    except:
+        raise ValueError("syntax error")
+    return item
+
+def convert_string_to_int(num):
+    if not num.isnumeric():        
         try:
-            num_b = question_in_list.pop(0)
+            num = int(num)
         except:
             raise ValueError("syntax error")
-            
-        if not num_b.isnumeric():
-            try:
-                num_b = int(num_b)
-            except:
-                raise ValueError("syntax error")
-            
-        num_c = 0
-        if operation == "plus":
-            num_c = int(num_a) + int(num_b)
-        if operation == "minus":
-            num_c = int(num_a) - int(num_b)
-        if operation == "multiplied":
-            num_c = int(num_a) * int(num_b)
-        if operation == "divided":
-            num_c = int(num_a) / int(num_b)
-        
-        num_a = num_c
+    return int(num)
 
-    return int(num_a)
+def evaluate(lhs_num, operation, rhs_num):
+    match operation:
+        case "plus":
+            return lhs_num + rhs_num
+        case "minus":
+            return lhs_num - rhs_num
+        case "multiplied":
+            return lhs_num * rhs_num
+        case "divided":
+            return lhs_num / rhs_num
