ba228735cdea47e51cf1fb03bcbcf838ba614a68
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-11 22:22:38 +0200

    Resistor Color Complete

---
+++ completed-easy/resistor-color/resistor_color.py
@@ -0,0 +1,7 @@
+colors_encoded = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+def color_code(color):
+    return colors_encoded.index(color)
+
+def colors():
+    return colors_encoded
