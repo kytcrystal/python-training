b7296aa4801a7a3a0fe04887afe939afb01e40b2
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-29 23:12:55 +0200

    on_earth function

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,3 +1,7 @@
 class SpaceAge:
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+        
+    def on_earth(self):
+        age = self.seconds / 31557600
+        return float("{:.2f}".format(age))
