045dcad55390e827eb37b092f5bb01d10b21ae45
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-04 23:18:30 +0200

    Check start and end brackets

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,12 +1,17 @@
 def is_paired(input_string):
-    brackets = ["[", "]", "(", ")", "{", "}"]
+    open_brackets = ["[", "(", "{"]
+    close_brackets = ["]", ")", "}"]
     filtered_string = ""
     
     for char in input_string:
-        if char in brackets:
+        if char in open_brackets or close_brackets:
             filtered_string += char
+    print(filtered_string)
             
     if len(filtered_string) % 2 != 0:
         return False
     
+    if filtered_string[0] not in open_brackets or filtered_string[-1] not in close_brackets:
+        return False
+        
     return True
