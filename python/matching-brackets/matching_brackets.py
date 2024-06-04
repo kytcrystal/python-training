df4cbd552e0db6b108867b1388ce4d51eebea785
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-04 23:25:38 +0200

    Check corresponding brackets

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -8,10 +8,20 @@ def is_paired(input_string):
             filtered_string += char
     print(filtered_string)
             
-    if len(filtered_string) % 2 != 0 or len(filtered_string) == 0:
+    if len(filtered_string) % 2 != 0:
         return False
     
-    if filtered_string[0] not in open_brackets or filtered_string[-1] not in close_brackets:
-        return False
+    while len(filtered_string) > 0:
+    
+        if filtered_string[0] not in open_brackets or filtered_string[-1] not in close_brackets:
+            return False
+        
+        if close_brackets[open_brackets.index(filtered_string[0])] == filtered_string[-1]:
+            filtered_string = filtered_string[1:-1]
+            
+        elif close_brackets[open_brackets.index(filtered_string[0])] == filtered_string[1]:
+            filtered_string = filtered_string[2:]
+        else:
+            return False
         
     return True
