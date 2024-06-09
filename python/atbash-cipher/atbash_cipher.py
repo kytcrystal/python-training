a7b0aed85adfc84075fcadfebd2b5122d8cdfc6f
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-09 22:38:16 +0200

    Atbash Cipher Encode Alphabet

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,5 +1,16 @@
+import string
+alphabet_list = list(string.ascii_lowercase)
+
 def encode(plain_text):
-    pass
+    ciphered_text = ""
+    count = 0
+    for char in plain_text.lower():
+        if char in alphabet_list:
+            index = alphabet_list.index(char)
+            ciphered_text += alphabet_list[-1-index]
+            count += 1
+    return ciphered_text
+        
 
 
 def decode(ciphered_text):
