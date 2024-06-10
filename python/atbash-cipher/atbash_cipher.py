b7d5f8b002dc78d9e9ef772544ee53b2f2168fce
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-10 22:39:51 +0200

    Atbash Cipher Encode numeric

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -8,8 +8,14 @@ def encode(plain_text):
         if char in alphabet_list:
             index = alphabet_list.index(char)
             ciphered_text += alphabet_list[-1-index]
+            count += 1        
+        if char.isnumeric():
+            ciphered_text += char
             count += 1
-    return ciphered_text
+        if count == 5:
+            ciphered_text += " "
+            count = 0
+    return ciphered_text.strip()
         
 
 
