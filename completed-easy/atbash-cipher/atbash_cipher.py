4e64e3dff5bac673f05f9c74113c05486ba1d050
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-11 22:55:28 +0200

    Atbash Cipher Refactored

--- completed-easy/atbash-cipher/atbash_cipher.py
+++ completed-easy/atbash-cipher/atbash_cipher.py
@@ -3,29 +3,25 @@ alphabet_list = list(string.ascii_lowercase)
 
 def encode(plain_text):
     ciphered_text = ""
-    count = 0
     for char in plain_text.lower():
-        if char in alphabet_list:
-            index = alphabet_list.index(char)
-            ciphered_text += alphabet_list[-1-index]
-            count += 1        
+        ciphered_text += determine_substitution(char)
         if char.isnumeric():
             ciphered_text += char
-            count += 1
-        if count == 5:
+        if len(ciphered_text) % 6 == 5:
             ciphered_text += " "
-            count = 0
     return ciphered_text.strip()
-        
-
 
 def decode(ciphered_text):
     plain_text = ""
     for char in ciphered_text:
-        if char in alphabet_list:
-            index = alphabet_list.index(char)
-            plain_text += alphabet_list[-1-index]
+        plain_text += determine_substitution(char)
         if char.isnumeric():
             plain_text += char
     return plain_text
 
+
+def determine_substitution(char):
+    if char in alphabet_list:
+        index = alphabet_list.index(char)
+        return alphabet_list[-1-index]
+    return ""
