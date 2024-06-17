572127ed1b91e07a23dbe38d3ac9c382e59e19e9
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-17 10:37:54 +0200

    Say Three Digits

--- python/say/say.py
+++ python/say/say.py
@@ -4,8 +4,7 @@ num_to_words = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
             15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
             19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty',
             50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
-            90: 'ninety', 100: 'hundred', 1000: 'thousand',
-            1000000: 'million,', 1000000000: 'billion'}
+            90: 'ninety'}
 
 
 def say(number):
@@ -21,9 +20,32 @@ def say(number):
     while number > 1:
         number_chunks = [number % 1000, *number_chunks]
         number = number // 1000
+    print(number_chunks)
     
     chunk = number_chunks[-1]
-    ones = chunk % 10
-    tens = chunk - ones
-    return num_to_words[tens] + "-" + num_to_words[ones]
+    say_words = ""
+    
+    if chunk >= 1000:
+        thousand = chunk // 1000
+        say_words += num_to_words[thousand] + " thousand "
+        chunk = chunk % 1000
+    
+    if chunk >= 100:
+        hundred = chunk // 100
+        say_words += num_to_words[hundred] + " hundred "
+        chunk = chunk % 100
+    
+    tens_flag = False
+    if chunk > 10:
+        tens = (chunk // 10) * 10
+        say_words += num_to_words[tens]
+        chunk = chunk % 10
+        tens_flag = True
+    
+    if chunk > 0:
+        if tens_flag:
+            say_words += "-"
+        say_words += num_to_words[chunk]
+        
+    return say_words.strip()
     
