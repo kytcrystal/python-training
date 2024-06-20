1617a4d12254b291bb327064d392084d0d8863d2
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-06-20 13:55:17 +0200

    feat: nth prime Complete

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -3,5 +3,21 @@ def prime(number):
         raise ValueError('there is no zeroth prime')
     if number < 0:
         raise ValueError('there is no negative prime')
+    
+    prime_number_count = 1
+    positive_number = 2
+    prime_number = 2
+    
+    while prime_number_count < number:
+        positive_number += 1
+        if is_prime(positive_number):
+            prime_number_count += 1
+            prime_number = positive_number
+        
+    return prime_number
 
-    return number
+def is_prime(number):
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            return False
+    return True
